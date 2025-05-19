b8d7ff861d23a17832564acb6be219007071fbd3
gadongmin <dmkim777@naver.com>
2025-05-19 15:35:45 +0900

    20250519(1)

--- src/echo/Client.java
+++ src/echo/Client.java
@@ -22,7 +22,7 @@ public class Client {
 		System.out.println("==================================");
 		System.out.println("[서버에 연결을 요청합니다.]");
 
-		socket.connect(new InetSocketAddress("192.168.0.96", 10001));
+		socket.connect(new InetSocketAddress("192.168.0.28", 10001));
 		System.out.println("[Server에 연결되었습니다.]");
 
 		// 메세지(쓰기 stream)
